4f9aa0823463d5f884a70b337373a6087f77fbdc
ChefJodlak <jodlaksde@gmail.com>
2018-12-03 18:24:22 +0100

    tesssty

--- install_auto.bash
+++ install_auto.bash
@@ -6,19 +6,19 @@
 # Github: https://github.com/ChefJodlak/GotowanieNaEkranie
 
 
-read -e -p "How many Servers do you want to create?: " n
+#read -e -p "How many Servers do you want to create?: " n
 
 
-sudo yum -y update
-sudo yum install -y squid 
-sudo yum install httpd-tools -y
-wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/ChefJodlak/GotowanieNaEkranie/master/config.conf --no-check-certificate
-sudo squid -z
-sudo chkconfig squid on
-touch /etc/squid/squid_access; htpasswd /etc/squid/squid_access admin
-sudo service squid start
+#sudo yum -y update
+#sudo yum install -y squid 
+#sudo yum install httpd-tools -y
+#wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/ChefJodlak/GotowanieNaEkranie/master/config.conf --no-check-certificate
+#sudo squid -z
+#sudo chkconfig squid on
+#touch /etc/squid/squid_access; htpasswd /etc/squid/squid_access admin
+#sudo service squid start
 
-for (( c=1; c<=120; c++ ))
+for (( c=1; c<=16; c++ ))
 do  
 	if(c<=8); then
 		yes | gcloud compute instances create proxyproxy$c --image-family=centos-6 --image-project=centos-cloud --tags=http-server,https-server --zone=europe-west4-a --metadata startup-script='#! /bin/bash
